852aa721c0e99f9299d05026457cdf8e844556f7
pontago <pontagoo+github@gmail.com>
2016-09-16 04:03:08 +0900

    - Swift3

--- ReactiveUI/ReactiveUI/NSTimer.swift
+++ ReactiveUI/ReactiveUI/NSTimer.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-public extension NSTimer {
+public extension Timer {
     
     // Big thanks to https://github.com/ashfurrow/Haste
-    class func scheduledTimerWithTimeInterval(seconds: NSTimeInterval, action: NSTimer -> (), repeats: Bool) -> NSTimer {
-        return scheduledTimerWithTimeInterval(seconds, target: self, selector: "_timerDidFire:", userInfo: RUITimerProxyTarget(action: action), repeats: repeats)
+    class func scheduledTimerWithTimeInterval(_ seconds: TimeInterval, action: @escaping (Timer) -> (), repeats: Bool) -> Timer {
+        return scheduledTimer(timeInterval: seconds, target: self, selector: #selector(Timer._timerDidFire(_:)), userInfo: RUITimerProxyTarget(action: action), repeats: repeats)
     }
     
 }
 
-internal extension NSTimer {
+internal extension Timer {
     
-    class func _timerDidFire(timer: NSTimer) {
+    class func _timerDidFire(_ timer: Timer) {
         if let proxyTarget = timer.userInfo as? RUITimerProxyTarget {
             proxyTarget.performAction(timer)
         }
@@ -28,13 +28,13 @@ internal extension NSTimer {
     typealias RUITimerProxyTargets = [String: RUITimerProxyTarget]
     
     class RUITimerProxyTarget : RUIProxyTarget {
-        var action: NSTimer -> ()
+        var action: (Timer) -> ()
         
-        init(action: NSTimer -> ()) {
+        init(action: @escaping (Timer) -> ()) {
             self.action = action
         }
         
-        func performAction(control: NSTimer) {
+        func performAction(_ control: Timer) {
             action(control)
         }
     }
@@ -52,7 +52,7 @@ internal extension NSTimer {
         }
     }
     
-    private func setProxyTargets(newValue: RUITimerProxyTarget) -> RUITimerProxyTarget {
+    fileprivate func setProxyTargets(_ newValue: RUITimerProxyTarget) -> RUITimerProxyTarget {
         objc_setAssociatedObject(self, &RUIProxyTargetsKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         return newValue
     }
